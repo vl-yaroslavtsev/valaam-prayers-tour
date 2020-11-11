c6dd077eba554e17b941735e3c8b9f1bf09ba117
unknown <vl.yaroslavtsev@gmail.com>
2020-11-11 21:40:32 +0300

    Доработка стилей navbar-collapse

--- src/js/app.js
+++ src/js/app.js
@@ -116,7 +116,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.14.14",
+	version: "1.14.15",
 	// theme: 'ios',
 
 	statusbar: {
