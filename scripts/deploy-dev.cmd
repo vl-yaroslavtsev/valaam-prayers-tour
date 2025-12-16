2ab1315f4d19a9b8c737a47a8c62e06ad7909130
Владимир Ярославцев <vl.yaroslavtsev@gmail.com>
2025-12-16 17:24:40 +0300

    Конфиг деплоя на dev

---
+++ scripts/deploy-dev.cmd
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001
+
+set KEY="C:\Users\Владимир\.ssh\id_rsa_dev.valaam.ru"
+set USER="dev.valaam.ru@dev.valaam.ru"
+set REMOTE_FOLDER="/var/www/dev.valaam.ru/data"
+
+ssh -i %KEY% %USER% "rm -rf %REMOTE_FOLDER%/prayers.f7/*"
+scp -r -i %KEY% ./dist/* %USER%:%REMOTE_FOLDER%/prayers.f7/
+ssh -i %KEY% %USER% "find %REMOTE_FOLDER%/prayers.f7/ -type d -exec chmod 4755 {} \;"
